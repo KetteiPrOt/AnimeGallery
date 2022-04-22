5524f2184b7b87302014f27373860cc1d7901d7e
Joel Mero <joelxxgamexx@gmail.com>
2022-04-22 15:07:28 -0500

    Version final 1

---
+++ script.js
@@ -0,0 +1,192 @@
+/*--------- Asignar Ids ----------*/
+// asigna las Ids a los botones y descripciones
+let descripciones = document.getElementsByClassName('weapon-description');
+for(desc in descripciones) {
+	descripciones[desc].id = 'descripcion_' + desc;
+};
+
+let botones = document.getElementsByClassName('button');
+for(btn in botones) {
+	botones[btn].id = 'boton_' + btn;
+};
+
+/* --------- Descripciones y botones ---------- */
+function alternar_0() {
+	if (descripciones[0].style.opacity == '') {
+		descripciones[0].style.opacity = '1';
+	} else {
+		descripciones[0].style.opacity = '';
+	}
+}
+botones[0].addEventListener('click', alternar_0);
+
+function alternar_1() {
+	if (descripciones[1].style.opacity == '') {
+		descripciones[1].style.opacity = '1';
+	} else {
+		descripciones[1].style.opacity = '';
+	}
+}
+botones[1].addEventListener('click', alternar_1);
+
+function alternar_2() {
+	if (descripciones[2].style.opacity == '') {
+		descripciones[2].style.opacity = '1';
+	} else {
+		descripciones[2].style.opacity = '';
+	}
+}
+botones[2].addEventListener('click', alternar_2);
+
+function alternar_3() {
+	if (descripciones[3].style.opacity == '') {
+		descripciones[3].style.opacity = '1';
+	} else {
+		descripciones[3].style.opacity = '';
+	}
+}
+botones[3].addEventListener('click', alternar_3);
+
+function alternar_4() {
+	if (descripciones[4].style.opacity == '') {
+		descripciones[4].style.opacity = '1';
+	} else {
+		descripciones[4].style.opacity = '';
+	}
+}
+botones[4].addEventListener('click', alternar_4);
+
+function alternar_5() {
+	if (descripciones[5].style.opacity == '') {
+		descripciones[5].style.opacity = '1';
+	} else {
+		descripciones[5].style.opacity = '';
+	}
+}
+botones[5].addEventListener('click', alternar_5);
+
+function alternar_6() {
+	if (descripciones[6].style.opacity == '') {
+		descripciones[6].style.opacity = '1';
+	} else {
+		descripciones[6].style.opacity = '';
+	}
+}
+botones[6].addEventListener('click', alternar_6);
+
+function alternar_7() {
+	if (descripciones[7].style.opacity == '') {
+		descripciones[7].style.opacity = '1';
+	} else {
+		descripciones[7].style.opacity = '';
+	}
+}
+botones[7].addEventListener('click', alternar_7);
+
+function alternar_8() {
+	if (descripciones[8].style.opacity == '') {
+		descripciones[8].style.opacity = '1';
+	} else {
+		descripciones[8].style.opacity = '';
+	}
+}
+botones[8].addEventListener('click', alternar_8);
+
+function alternar_9() {
+	if (descripciones[9].style.opacity == '') {
+		descripciones[9].style.opacity = '1';
+	} else {
+		descripciones[9].style.opacity = '';
+	}
+}
+botones[9].addEventListener('click', alternar_9);
+
+function alternar_10() {
+	if (descripciones[10].style.opacity == '') {
+		descripciones[10].style.opacity = '1';
+	} else {
+		descripciones[10].style.opacity = '';
+	}
+}
+botones[10].addEventListener('click', alternar_10);
+
+function alternar_11() {
+	if (descripciones[11].style.opacity == '') {
+		descripciones[11].style.opacity = '1';
+	} else {
+		descripciones[11].style.opacity = '';
+	}
+}
+botones[11].addEventListener('click', alternar_11);
+
+function alternar_12() {
+	if (descripciones[12].style.opacity == '') {
+		descripciones[12].style.opacity = '1';
+	} else {
+		descripciones[12].style.opacity = '';
+	}
+}
+botones[12].addEventListener('click', alternar_12);
+
+function alternar_13() {
+	if (descripciones[13].style.opacity == '') {
+		descripciones[13].style.opacity = '1';
+	} else {
+		descripciones[13].style.opacity = '';
+	}
+}
+botones[13].addEventListener('click', alternar_13);
+
+function alternar_14() {
+	if (descripciones[14].style.opacity == '') {
+		descripciones[14].style.opacity = '1';
+	} else {
+		descripciones[14].style.opacity = '';
+	}
+}
+botones[14].addEventListener('click', alternar_14);
+
+function alternar_15() {
+	if (descripciones[15].style.opacity == '') {
+		descripciones[15].style.opacity = '1';
+	} else {
+		descripciones[15].style.opacity = '';
+	}
+}
+botones[15].addEventListener('click', alternar_15);
+
+function alternar_16() {
+	if (descripciones[16].style.opacity == '') {
+		descripciones[16].style.opacity = '1';
+	} else {
+		descripciones[16].style.opacity = '';
+	}
+}
+botones[16].addEventListener('click', alternar_16);
+
+function alternar_17() {
+	if (descripciones[17].style.opacity == '') {
+		descripciones[17].style.opacity = '1';
+	} else {
+		descripciones[17].style.opacity = '';
+	}
+}
+botones[17].addEventListener('click', alternar_17);
+
+function alternar_18() {
+	if (descripciones[18].style.opacity == '') {
+		descripciones[18].style.opacity = '1';
+	} else {
+		descripciones[18].style.opacity = '';
+	}
+}
+botones[18].addEventListener('click', alternar_18);
+
+function alternar_19() {
+	if (descripciones[19].style.opacity == '') {
+		descripciones[19].style.opacity = '1';
+	} else {
+		descripciones[19].style.opacity = '';
+	}
+}
+botones[19].addEventListener('click', alternar_19);
